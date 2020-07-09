d18ff7a7ea150f07024d6bb585830a8b9c715347
LeagueLugas <leaguelugas@gmail.com>
2020-07-09 23:37:40 +0900

    [ADD] - project

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/ShankApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dsm_scarfs.shank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/dsm_scarfs/shank/ShankApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dsm_scarfs.shank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
