d487d79504482953e14dd73812641677bf1aea40
Ryua <machiro119@naver.com>
2020-07-14 22:16:49 +0900

    [ADD] - Entity
    add student entity

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/student/student.java
@@ -0,0 +1,27 @@
+package kr.hs.dsm_scarfs.shank.entites.student;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String studentNumber;
+
+    private String password;
+
+    private String name;
+}
