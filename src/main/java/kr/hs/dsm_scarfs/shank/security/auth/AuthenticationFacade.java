43e9a8552480021b4cebb12a3dcd6b773cc09c8c
Ryua <machiro119@naver.com>
2020-07-16 15:20:42 +0900

    [ADD] - authentication facade

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/security/auth/AuthenticationFacade.java
@@ -0,0 +1,18 @@
+package kr.hs.dsm_scarfs.shank.security.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade {
+
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public String getUserEmail() {
+        return this.getAuthentication().getName();
+    }
+
+}
