944a8bcbb274bb64cd84350515c49499ce8a43d0
Ryua <machiro119@naver.com>
2020-07-16 15:16:24 +0900

    [ADD] - refresh token service

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/refresh_token/RefreshToken.java
@@ -0,0 +1,30 @@
+package kr.hs.dsm_scarfs.shank.entites.refresh_token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+
+@Getter
+@RedisHash(value = "refresh_token")
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    private String email;
+
+    @Indexed
+    private String refreshToken;
+
+    @Indexed
+    private Long ttl;
+
+    public RefreshToken update(String refreshToken, Long ttl) {
+        this.refreshToken = refreshToken;
+        this.ttl = ttl;
+        return this;
+    }
+
+}
