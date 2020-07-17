e7d606cd6e3724884324cb4f42925df74af0aa33
Ryua <machiro119@naver.com>
2020-07-17 20:06:05 +0900

    [FIX] - add orm method

--- src/main/java/kr/hs/dsm_scarfs/shank/entites/notice/repository/NoticeRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/notice/repository/NoticeRepository.java
@@ -1,11 +1,12 @@
 package kr.hs.dsm_scarfs.shank.entites.notice.repository;
 
 import kr.hs.dsm_scarfs.shank.entites.notice.Notice;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NoticeRepository extends CrudRepository<Notice, Integer> {
-
+    Page<Notice> findAllBy(Pageable page);
 }
