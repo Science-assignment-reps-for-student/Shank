d0bb0d6f7429a57e8e0e17b44b6bab5907ed5189
Ryua <machiro119@naver.com>
2020-07-17 12:14:37 +0900

    [ADD] - notice repository

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/notice/repository/NoticeRepository.java
@@ -0,0 +1,11 @@
+package kr.hs.dsm_scarfs.shank.entites.notice.repository;
+
+import kr.hs.dsm_scarfs.shank.entites.notice.Notice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends CrudRepository<Notice, Integer> {
+
+}
