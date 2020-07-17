014252a696379901aca18b1a16de0ba6c02a7293
Ryua <machiro119@naver.com>
2020-07-17 20:04:32 +0900

    [FIX] - HOT SIX

--- src/main/java/kr/hs/dsm_scarfs/shank/controllers/HomeworkController.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/controllers/HomeworkController.java
@@ -1,7 +1,6 @@
 package kr.hs.dsm_scarfs.shank.controllers;
 
 import kr.hs.dsm_scarfs.shank.payload.response.HomeworkListResponse;
-import kr.hs.dsm_scarfs.shank.payload.response.HomeworkResponse;
 import kr.hs.dsm_scarfs.shank.service.homework.HomeworkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/homework")
--- src/main/java/kr/hs/dsm_scarfs/shank/entites/homework/repository/HomeworkRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/homework/repository/HomeworkRepository.java
@@ -3,7 +3,6 @@ package kr.hs.dsm_scarfs.shank.entites.homework.repository;
 import kr.hs.dsm_scarfs.shank.entites.homework.Homework;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
