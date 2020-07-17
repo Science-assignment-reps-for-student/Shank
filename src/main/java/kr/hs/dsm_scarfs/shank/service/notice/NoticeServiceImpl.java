1d10ce7e7c94a165472ddd0b469593a40aca8d1d
Ryua <machiro119@naver.com>
2020-07-17 20:05:10 +0900

    [ADD] - notice service

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/service/notice/NoticeServiceImpl.java
@@ -0,0 +1,47 @@
+package kr.hs.dsm_scarfs.shank.service.notice;
+
+import kr.hs.dsm_scarfs.shank.entites.notice.Notice;
+import kr.hs.dsm_scarfs.shank.entites.notice.repository.NoticeRepository;
+
+import kr.hs.dsm_scarfs.shank.payload.response.NoticeListResponse;
+import kr.hs.dsm_scarfs.shank.payload.response.NoticeResponse;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeServiceImpl implements NoticeService{
+
+    private final NoticeRepository noticeRepository;
+
+    @Override
+    public NoticeListResponse getNoticeList(Pageable page) {
+        Page<Notice> noticePage = noticeRepository.findAllBy(page);
+
+        List<NoticeResponse> noticeResponses = new ArrayList<>();
+
+        for (Notice notice : noticePage) {
+            noticeResponses.add(
+                    NoticeResponse.builder()
+                    .title(notice.getTitle())
+                    .createdAt(notice.getCreatedAt())
+                    .preViewContent(notice.getContent().substring(0, Math.min(150, notice.getContent().length())))
+                    .build()
+            );
+        }
+
+
+        return NoticeListResponse.builder()
+                .totalElements((int) noticePage.getTotalElements())
+                .totalPages(noticePage.getTotalPages())
+                .noticeResponses(noticeResponses)
+                .build();
+    }
+
+}
