9ca3cb6941ed239a8321b2ea1ea38d14f83ba062
Ryua <machiro119@naver.com>
2020-07-18 13:57:18 +0900

    [ADD] - board service

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/service/board/BoardService.java
@@ -0,0 +1,8 @@
+package kr.hs.dsm_scarfs.shank.service.board;
+
+import kr.hs.dsm_scarfs.shank.payload.response.BoardListResponse;
+import org.springframework.data.domain.Pageable;
+
+public interface BoardService {
+    BoardListResponse getBoardList(Pageable page);
+}
