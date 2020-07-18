8aaf0470975865ad87d5ae4001f4056d28a30fe7
Ryua <machiro119@naver.com>
2020-07-18 14:59:50 +0900

    [ADD] - member controller

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/controllers/TeamController.java
@@ -0,0 +1,31 @@
+package kr.hs.dsm_scarfs.shank.controllers;
+
+import kr.hs.dsm_scarfs.shank.payload.request.TeamRequest;
+import kr.hs.dsm_scarfs.shank.payload.response.TeamResponse;
+import kr.hs.dsm_scarfs.shank.service.team.TeamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/team")
+@RequiredArgsConstructor
+public class TeamController {
+
+    private final TeamService teamService;
+
+    @GetMapping("{homeworkId}")
+    public TeamResponse getTeam(@PathVariable Integer homeworkId) {
+        return teamService.getTeam(homeworkId);
+    }
+
+    @PostMapping
+    public void addTeam(@RequestBody TeamRequest teamRequest) {
+        teamService.addTeam(teamRequest);
+    }
+
+    @DeleteMapping("/{teamId}")
+    public void deleteTeam(@PathVariable Integer teamId) {
+        teamService.deleteTeam(teamId);
+    }
+
+}
