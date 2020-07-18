8c14a97040f679debce943554914ea4f7b2ba84c
Ryua <machiro119@naver.com>
2020-07-18 22:09:55 +0900

    [FIX] - add orm

--- src/main/java/kr/hs/dsm_scarfs/shank/entites/member/repository/MemberRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/member/repository/MemberRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Integer> {
-    Member findByStudentIdAndHomeworkId(Integer studentId, Integer homeworkId);
+    Optional<Member> findByStudentIdAndHomeworkId(Integer studentId, Integer homeworkId);
     List<Member> findAllByTeamId(Integer teamId);
     void deleteAllByTeamId(Integer teamId);
 }
