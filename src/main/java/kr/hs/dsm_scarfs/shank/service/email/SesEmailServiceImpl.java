d5a60fc61407a1d6e52b3032d15779f88c6eab3d
Ryua <machiro119@naver.com>
2020-07-20 16:48:46 +0900

    [FIX] - //

--- src/main/java/kr/hs/dsm_scarfs/shank/service/email/SesEmailServiceImpl.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/service/email/SesEmailServiceImpl.java
@@ -10,20 +10,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SesEmailServiceImpl implements EmailService{
 
-    private final AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync;
+    //private final AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync;
 
     private static final String UTF_8_ENCODED_SOURCE_NAME = "=?utf-8?B?U0NBUkZT?=";
 
     @Override
     public void sendEmail(String receiveEmail, String code) {
 
-        SendTemplatedEmailRequest request = new SendTemplatedEmailRequest()
-                .withDestination(new Destination().withToAddresses(receiveEmail))
-                .withTemplate("ScarfsEmailConfirmTemplate")
-                .withSource(UTF_8_ENCODED_SOURCE_NAME + "<?>")
-                .withTemplateData("{\"code\": \"" + code + "\", \"email\": \"" + receiveEmail + "\"}");
-
-        amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
+//        SendTemplatedEmailRequest request = new SendTemplatedEmailRequest()
+//                .withDestination(new Destination().withToAddresses(receiveEmail))
+//                .withTemplate("ScarfsEmailConfirmTemplate")
+//                .withSource(UTF_8_ENCODED_SOURCE_NAME + "<?>")
+//                .withTemplateData("{\"code\": \"" + code + "\", \"email\": \"" + receiveEmail + "\"}");
+//
+//        amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
     }
 
 }
