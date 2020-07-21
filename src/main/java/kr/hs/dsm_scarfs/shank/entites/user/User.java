9a261291ad0751c76a240dbe5786e1cc8964bcc5
LeagueLugas <leaguelugas@gmail.com>
2020-07-21 15:36:24 +0900

    [ADD] - user factory class

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/user/User.java
@@ -0,0 +1,10 @@
+package kr.hs.dsm_scarfs.shank.entites.user;
+
+public interface User {
+    Integer getId();
+    String getEmail();
+    String getPassword();
+    String getName();
+    String getStudentNumber();
+    String getStudentClassNumber();
+}
