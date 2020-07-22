311b0b4c678ec98cf514eda7971c46c33ab56b1c
Ryua <machiro119@naver.com>
2020-07-22 23:42:26 +0900

    [FIX] - add get homework

--- src/main/java/kr/hs/dsm_scarfs/shank/controllers/HomeworkController.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/controllers/HomeworkController.java
@@ -1,10 +1,12 @@
 package kr.hs.dsm_scarfs.shank.controllers;
 
+import kr.hs.dsm_scarfs.shank.payload.response.HomeworkContentResponse;
 import kr.hs.dsm_scarfs.shank.payload.response.HomeworkListResponse;
 import kr.hs.dsm_scarfs.shank.service.homework.HomeworkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class HomeworkController {
     @GetMapping
     public HomeworkListResponse homeworkList(Pageable page) {
         return homeworkService.getHomeworkList(page);
+    }
+
+    @GetMapping("{/homeworkId}")
+    public HomeworkContentResponse getHomeworkContent(@PathVariable Integer homeworkId) {
+        return homeworkService.getHomeworkContent(homeworkId);
     }
 }
