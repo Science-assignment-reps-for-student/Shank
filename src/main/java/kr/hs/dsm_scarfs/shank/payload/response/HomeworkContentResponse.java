82245f65cf8c392f5e51ed9e223e36c2b45b8072
Ryua <machiro119@naver.com>
2020-07-22 23:43:22 +0900

    [ADD] - homework response

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/payload/response/HomeworkContentResponse.java
@@ -0,0 +1,27 @@
+package kr.hs.dsm_scarfs.shank.payload.response;
+
+import kr.hs.dsm_scarfs.shank.entites.homework.enums.HomeworkType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class HomeworkContentResponse {
+
+    private String title;
+
+    private HomeworkType type;
+
+    private LocalDateTime createdAt;
+
+    private LocalDate deadLine;
+
+    private String content;
+
+    private Integer view;
+
+    private boolean isComplete;
+}
