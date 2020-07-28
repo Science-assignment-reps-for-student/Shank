0bdcc9b98e0cbc1db33a566985913be50f732ae0
Ryua <machiro119@naver.com>
2020-07-28 14:35:03 +0900

    [FIX] - add get controller

--- src/main/java/kr/hs/dsm_scarfs/shank/service/homework/HomeworkServiceImpl.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/service/homework/HomeworkServiceImpl.java
@@ -20,14 +20,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
