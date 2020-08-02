eb341a7353ba2229d9ec52324c0fcc169e879a21
Ryua <machiro119@naver.com>
2020-08-03 08:58:22 +0900

    [FIX] - add orm

--- src/main/java/kr/hs/dsm_scarfs/shank/entites/homework/repository/HomeworkRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/homework/repository/HomeworkRepository.java
@@ -1,12 +1,14 @@
 package kr.hs.dsm_scarfs.shank.entites.homework.repository;
 
 import kr.hs.dsm_scarfs.shank.entites.homework.Homework;
+import kr.hs.dsm_scarfs.shank.entites.notice.Notice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface HomeworkRepository extends CrudRepository<Homework, Integer> {
@@ -20,4 +22,8 @@ public interface HomeworkRepository extends CrudRepository<Homework, Integer> {
     Page<Homework> findAllByDeadline4After(Pageable page, LocalDate date);
 
     Page<Homework> findAllByTitleContainsOrContentContains(String tileQuery, String contentQuery, Pageable page);
+
+    Optional<Homework> findTop1ByIdBeforeOrderByIdAsc(Integer id);
+
+    Optional<Homework> findTop1ByIdAfterOrderByIdAsc(Integer id);
 }
