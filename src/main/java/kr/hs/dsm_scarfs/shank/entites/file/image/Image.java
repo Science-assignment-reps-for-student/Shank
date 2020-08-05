92c9929431d01457cbf77740edfbe43141809447
Ryua <machiro119@naver.com>
2020-08-06 08:41:56 +0900

    [FIX] - my mistake

---
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/file/image/Image.java
@@ -0,0 +1,26 @@
+package kr.hs.dsm_scarfs.shank.entites.file.image;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer boardId;
+
+    private String fileName;
+
+}
