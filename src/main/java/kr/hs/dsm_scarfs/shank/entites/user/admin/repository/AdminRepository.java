c568d671d36674234af72280ac10aec8473102ea
LeagueLugas <leaguelugas@gmail.com>
2020-08-11 13:45:36 +0900

    [FIX] - user repositories

--- src/main/java/kr/hs/dsm_scarfs/shank/entites/user/admin/repository/AdminRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/user/admin/repository/AdminRepository.java
@@ -1,12 +1,18 @@
 package kr.hs.dsm_scarfs.shank.entites.user.admin.repository;
 
+import kr.hs.dsm_scarfs.shank.entites.user.User;
 import kr.hs.dsm_scarfs.shank.entites.user.admin.Admin;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AdminRepository extends CrudRepository<Admin, Integer> {
     Optional<Admin> findByEmail(String email);
+
+    @Query(value = "select a.* from admin as a inner join message as m on m.admin_id=a.id where m.student_id=?1 group by a.id", nativeQuery = true)
+    List<Admin> getChattedAdmin(Integer studentId);
 }
