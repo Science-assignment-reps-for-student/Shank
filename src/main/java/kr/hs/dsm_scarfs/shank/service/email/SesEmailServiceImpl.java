3930f63d6f41daeb47fa4f03aa15aa23fd32c17a
Ryua <machiro119@naver.com>
2020-08-13 19:31:46 +0900

    [FIX] - change code

--- src/main/java/kr/hs/dsm_scarfs/shank/service/email/SesEmailServiceImpl.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/service/email/SesEmailServiceImpl.java
@@ -1,29 +1,41 @@
 package kr.hs.dsm_scarfs.shank.service.email;
 
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsync;
-import com.amazonaws.services.simpleemail.model.Destination;
-import com.amazonaws.services.simpleemail.model.SendTemplatedEmailRequest;
 import lombok.RequiredArgsConstructor;
+import net.sargue.mailgun.Configuration;
+import net.sargue.mailgun.Mail;
+import net.sargue.mailgun.Response;
+import net.sargue.mailgun.content.Body;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SesEmailServiceImpl implements EmailService{
 
-    //private final AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync;
+    @Value("${spring.email.domain}")
+    private String domain;
 
-    private static final String UTF_8_ENCODED_SOURCE_NAME = "=?utf-8?B?U0NBUkZT?=";
+    @Value("${spring.email.apikey}")
+    private String apiKey;
 
     @Override
     public void sendEmail(String receiveEmail, String code) {
+        Configuration configuration = new Configuration()
+                .domain(domain)
+                .apiKey(apiKey)
+                .from("Avocat", "avocat@dsm.hs.kr");
 
-//        SendTemplatedEmailRequest request = new SendTemplatedEmailRequest()
-//                .withDestination(new Destination().withToAddresses(receiveEmail))
-//                .withTemplate("ScarfsEmailConfirmTemplate")
-//                .withSource(UTF_8_ENCODED_SOURCE_NAME + "<?>")
-//                .withTemplateData("{\"code\": \"" + code + "\", \"email\": \"" + receiveEmail + "\"}");
-//
-//        amazonSimpleEmailServiceAsync.sendTemplatedEmailAsync(request);
+        Body builder = new Body("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n" +
+                "<html xmlns=\"http://www.w3.org/1999/xhtml\"><body><p style=\"color: red\">Test</p></body></html>", "");
+
+        Response response = Mail.using(configuration)
+                .to(receiveEmail)
+                .subject("아보캣 인증 메일")
+                .content(builder)
+                .build()
+                .send();
+
+
     }
 
 }
