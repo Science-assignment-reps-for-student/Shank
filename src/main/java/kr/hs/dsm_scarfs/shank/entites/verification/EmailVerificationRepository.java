82200f19cc05e6cb8e3534d990b2a52c17ff60eb
LeagueLugas <leaguelugas@gmail.com>
2020-08-19 08:34:11 +0900

    [FIX] - change super repository

--- src/main/java/kr/hs/dsm_scarfs/shank/entites/verification/EmailVerificationRepository.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/entites/verification/EmailVerificationRepository.java
@@ -1,9 +1,9 @@
 package kr.hs.dsm_scarfs.shank.entites.verification;
 
 
-import org.springframework.data.keyvalue.repository.KeyValueRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmailVerificationRepository extends KeyValueRepository<EmailVerification, String> {
+public interface EmailVerificationRepository extends CrudRepository<EmailVerification, String> {
 }
