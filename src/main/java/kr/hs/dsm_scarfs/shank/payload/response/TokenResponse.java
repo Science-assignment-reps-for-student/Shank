36e51540a2c0d3c48befd578d261c78334275117
Ryua <machiro119@naver.com>
2020-08-20 22:23:26 +0900

    [FIX] - my mistake

--- src/main/java/kr/hs/dsm_scarfs/shank/payload/response/TokenResponse.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/payload/response/TokenResponse.java
@@ -2,8 +2,10 @@ package kr.hs.dsm_scarfs.shank.payload.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TokenResponse {
 
