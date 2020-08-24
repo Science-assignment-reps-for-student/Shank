4026b7d69eeacf02d196ca7366a3c1fe92d1dce4
LeagueLugas <leaguelugas@gmail.com>
2020-08-24 15:48:09 +0900

    [FIX] - security

--- src/main/java/kr/hs/dsm_scarfs/shank/security/SecurityConfig.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().disable()
                 .formLogin().disable()
                 .authorizeRequests()
                     .antMatchers("/user/**").permitAll()
