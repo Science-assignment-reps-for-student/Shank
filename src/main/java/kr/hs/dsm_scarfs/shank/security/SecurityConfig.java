4d507b445746d44727a53c86b22d3cee030d5540
LeagueLugas <leaguelugas@gmail.com>
2020-08-24 19:49:52 +0900

    [FIX] - cors config

--- src/main/java/kr/hs/dsm_scarfs/shank/security/SecurityConfig.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
@@ -21,7 +22,7 @@ import java.util.Collections;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -35,26 +36,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .cors().disable()
+                .cors().and()
                 .sessionManagement().disable()
                 .formLogin().disable()
                 .authorizeRequests()
-                    .antMatchers("/auth").permitAll()
-                    .antMatchers("/user/**").permitAll()
+                    .antMatchers("/v2/shank/auth").permitAll()
+                    .antMatchers("/v2/shank/user/me").authenticated()
+                    .antMatchers("/v2/shank/user/**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
                     .apply(new JwtConfigurer(jwtTokenProvider)).and()
                     .apply(new ExceptionConfigurer());
     }
 
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 
     @Bean
