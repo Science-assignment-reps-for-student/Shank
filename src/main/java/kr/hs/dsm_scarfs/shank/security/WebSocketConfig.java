101fd70022add47a6f208f892ac16dc8d6af3a3c
LeagueLugas <leaguelugas@gmail.com>
2020-08-25 12:03:37 +0900

    [FIX] - web socket config

--- src/main/java/kr/hs/dsm_scarfs/shank/security/WebSocketConfig.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/security/WebSocketConfig.java
@@ -2,13 +2,13 @@ package kr.hs.dsm_scarfs.shank.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -19,4 +19,5 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket").setAllowedOrigins("*").withSockJS();
     }
+    
 }
