3d5cbe34fa4722ed90f45be31f595d0afaae5f02
LeagueLugas <leaguelugas@gmail.com>
2020-08-26 18:02:11 +0900

    [FIX] - message service url

--- src/main/java/kr/hs/dsm_scarfs/shank/controllers/MessageController.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/controllers/MessageController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/message")
+@RestController
+@RequestMapping("/message")
 @RequiredArgsConstructor
 public class MessageController {
 
