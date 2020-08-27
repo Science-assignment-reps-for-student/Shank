6aa098ee01ee366f18ae87c8593f0897d0769972
LeagueLugas <leaguelugas@gmail.com>
2020-08-27 21:24:46 +0900

    [FIX] - socket config

--- src/main/java/kr/hs/dsm_scarfs/shank/security/SocketConfig.java
+++ src/main/java/kr/hs/dsm_scarfs/shank/security/SocketConfig.java
@@ -1,19 +1,33 @@
 package kr.hs.dsm_scarfs.shank.security;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Configuration
 public class SocketConfig {
 
+    private SocketIOServer server;
+
     @Bean
     public SocketIOServer webSocketServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        //config.setOrigin("*:*");
         config.setPort(8001);
 
-        return new SocketIOServer(config);
+        SocketIOServer server = new SocketIOServer(config);
+        server.start();
+        this.server = server;
+
+        return server;
+    }
+
+    @PreDestroy
+    public void socketStop() {
+        server.stop();
     }
 
 }
